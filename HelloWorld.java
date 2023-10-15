ffcfe0d233cffa37d0544f62f2fee396465c6bb3
ADNS666 <147993684+ADNS666@users.noreply.github.com>
2023-10-15 22:56:10 +0800

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,18 @@
+/*
+1.注释
+*/
+
+// 2.单行注释
+
+/**
+*3.类的注释信息
+*
+*
+*/
+
+public class HelloWorld{
+	public static void main(String[] args){
+		System.out.println("Hello World");
+		System.out.println("cool");
+	}
+}
